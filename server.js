f4a11125c978d406f2c490a015ac3b78c2697dcb
Erik Wiklund <85eriwik@skola.boras.se>
2021-08-18 13:35:29 +0200

    Added server.js with initial settings to start

---
+++ server.js
@@ -0,0 +1,15 @@
+const exp = require('constants');
+const express = require('express');
+
+// Create server application
+const app = express();
+
+// Parse incoming json
+app.use(express.json());
+
+// Add resources
+
+// Error handlers
+
+// Start the server
+app.listen(3000);
